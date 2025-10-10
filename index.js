8ce0b277cfc7a479aa004b35c8940a2fddb716a3
TechCrent <ciicicrentsil@gmail.com>
2025-10-10 10:16:29 +0000

    initial js push

---
+++ index.js
@@ -0,0 +1,21 @@
+const div = 0;
+const mult = 0;
+const minus = 0;
+const plus = 0;
+
+
+const equal = 0;
+const clear = 0;
+const del = 0;
+
+const one = 0;
+const two = 0;
+const three = 0;
+const four = 0;
+const five = 0;
+const six = 0;
+const seven = 0;
+const eight = 0;
+const nine = 0;
+const zer = 0;
+
