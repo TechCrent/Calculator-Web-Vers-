ac142bc01be742c7f69e6fb12005d60abaf8e9d2
TechCrent <ciicicrentsil@gmail.com>
2025-10-10 10:49:44 +0000

    JavaScript Structure(Building)

--- index.js
+++ index.js
@@ -1,21 +1,54 @@
-const div = 0;
-const mult = 0;
-const minus = 0;
-const plus = 0;
+
+//Main Function
+one.addEventListener("click",function(){
+    if (screen.innerText > 0){
+        screen.innerText += "1";
+    }else{
+        screen.innerText = "1";
+    }
+});
+
+//Array Idea
+numbers=[zero,one,two,three,four,five,six,seven,eight,nine];
 
 
-const equal = 0;
-const clear = 0;
-const del = 0;
+const screen = document.querySelector(".scr");
 
-const one = 0;
-const two = 0;
-const three = 0;
-const four = 0;
-const five = 0;
-const six = 0;
-const seven = 0;
-const eight = 0;
-const nine = 0;
-const zer = 0;
+
+const div = document.querySelector(".div");
+
+const mul = document.querySelector(".mul");
+
+const minus = document.querySelector(".minus");
+
+const plus = document.querySelector(".plus");
+
+
+const equal = document.querySelector(".equal");
+
+const clear = document.querySelector(".clear");
+
+const del = document.querySelector(".del");
+
+
+
+const one = document.querySelector(".one");
+
+const two = document.querySelector(".two");
+
+const three = document.querySelector(".three");
+
+const four = document.querySelector(".four");
+
+const five = document.querySelector(".five");
+
+const six = document.querySelector(".six");
+
+const seven = document.querySelector(".seven");
+
+const eight = document.querySelector(".eight");
+
+const nine = document.querySelector(".nine");
+
+const zero = document.querySelector(".zer");
 
